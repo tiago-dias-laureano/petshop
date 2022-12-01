f15286677333112b638bec884099871a1ca9d410
Tiago Dias Laureano <tiagodiaslaureano32@gmail.com>
2022-12-01 16:37:13 -0300

    Add files via upload

---
+++ src/migrations/20221201190402-create-status.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('status', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+     });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('status');
+  }
+};
